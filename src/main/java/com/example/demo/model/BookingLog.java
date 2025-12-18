687a3f9d4da8b892d3b345f3fdb61e36b926b044
bala-amypo <bala@amypo.in>
2025-12-18 08:10:23 +0000

    User commit

--- src/main/java/com/example/demo/model/BookingLog.java
+++ src/main/java/com/example/demo/model/BookingLog.java
@@ -1,80 +1,45 @@
 package com.example.demo.model;
 
-import javax.persistence.*;
+
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Objects;
+
 
 @Entity
 public class BookingLog {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "booking_id", nullable = false)
-    private Booking booking;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
 
-    private String logMessage;
 
-    private LocalDateTime loggedAt;
+@ManyToOne
+private Booking booking;
 
-    public BookingLog() {}
 
-    public BookingLog(Long id, Booking booking, String logMessage, LocalDateTime loggedAt) {
-        this.id = id;
-        this.booking = booking;
-        this.logMessage = logMessage;
-        this.loggedAt = loggedAt;
-    }
+private String logMessage;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+private LocalDateTime loggedAt;
 
-    public Booking getBooking() {
-        return booking;
-    }
 
-    public void setBooking(Booking booking) {
-        this.booking = booking;
-    }
+@PrePersist
+public void onCreate() {
+this.loggedAt = LocalDateTime.now();
+}
 
-    public String getLogMessage() {
-        return logMessage;
-    }
 
-    public void setLogMessage(String logMessage) {
-        this.logMessage = logMessage;
-    }
+public BookingLog() {}
 
-    public LocalDateTime getLoggedAt() {
-        return loggedAt;
-    }
 
-    public void setLoggedAt(LocalDateTime loggedAt) {
-        this.loggedAt = loggedAt;
-    }
+public BookingLog(Long id, Booking booking, String logMessage, LocalDateTime loggedAt) {
+this.id = id;
+this.booking = booking;
+this.logMessage = logMessage;
+this.loggedAt = loggedAt;
+}
 
-    @PrePersist
-    public void onCreate() {
-        this.loggedAt = LocalDateTime.now();
-    }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BookingLog that = (BookingLog) o;
-        return Objects.equals(id, that.id) && Objects.equals(booking, that.booking);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, booking);
-    }
+// getters and setters
 }
