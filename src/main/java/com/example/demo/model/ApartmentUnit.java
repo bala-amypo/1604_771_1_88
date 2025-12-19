feeda138a5651563afd22085ff14f94d0dc6ee06
bala-amypo <bala@amypo.in>
2025-12-19 08:05:51 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/ApartmentUnit.java
@@ -0,0 +1,43 @@
+package com.example.demo.model;
+
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class ApartmentUnit {
+
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+
+private String unitNumber;
+
+
+private Integer floor;
+
+
+@OneToOne
+@JoinColumn(name = "owner_id")
+private User owner;
+
+
+public ApartmentUnit() {}
+
+
+public ApartmentUnit(Long id, String unitNumber, Integer floor, User owner) {
+this.id = id;
+this.unitNumber = unitNumber;
+this.floor = floor;
+this.owner = owner;
+}
+
+
+public void setOwner(User user) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'setOwner'");
+}
+
+}
