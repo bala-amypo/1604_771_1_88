9e7c8746bc8f725e167be91d2390f3f1b1cf5561
bala-amypo <bala@amypo.in>
2025-12-19 08:08:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/ApartmentUnitServiceImpl.java
@@ -0,0 +1,43 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.ApartmentUnit;
+import com.example.demo.model.User;
+import com.example.demo.repository.ApartmentUnitRepository;
+import com.example.demo.repository.UserRepository;
+import com.example.demo.service.ApartmentUnitService;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApartmentUnitServiceimpl implements ApartmentUnitService {
+
+    private final ApartmentUnitRepository apartmentUnitRepository;
+    private final UserRepository userRepository;
+
+    public ApartmentUnitServiceimpl(ApartmentUnitRepository apartmentUnitRepository,
+                                    UserRepository userRepository) {
+        this.apartmentUnitRepository = apartmentUnitRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public ApartmentUnit assignUnitToUser(Long userId, ApartmentUnit unit) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+        unit.setOwner(user);
+        user.setApartmentUnit(unit);
+
+        return apartmentUnitRepository.save(unit);
+    }
+
+    @Override
+    public ApartmentUnit getUnitByUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+        return apartmentUnitRepository.findByOwner(user)
+                .orElseThrow(() -> new ResourceNotFoundException("Apartment unit not found"));
+    }
+}
