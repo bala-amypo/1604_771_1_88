e5f1108461e9a448a333d1297e0bed1b100b2780
bala-amypo <bala@amypo.in>
2025-12-19 04:50:52 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/BookingLogServiceimpl.java
@@ -0,0 +1,47 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.exception.ResourceNotFoundException;
+import com.example.demo.model.Booking;
+import com.example.demo.model.BookingLog;
+import com.example.demo.repository.BookingLogRepository;
+import com.example.demo.repository.BookingRepository;
+import com.example.demo.service.BookingLogService;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class BookingLogServiceimpl implements BookingLogService {
+
+    private final BookingLogRepository bookingLogRepository;
+    private final BookingRepository bookingRepository;
+
+    public BookingLogServiceimpl(BookingLogRepository bookingLogRepository,
+                                 BookingRepository bookingRepository) {
+        this.bookingLogRepository = bookingLogRepository;
+        this.bookingRepository = bookingRepository;
+    }
+
+    @Override
+    public BookingLog addLog(Long bookingId, String message) {
+        Booking booking = bookingRepository.findById(bookingId)
+                .orElseThrow(() -> new ResourceNotFoundException("Booking not found"));
+
+        BookingLog log = new BookingLog();
+        log.setBooking(booking);
+        log.setLogMessage(message);
+        log.setLoggedAt(LocalDateTime.now());
+
+        return bookingLogRepository.save(log);
+    }
+
+    @Override
+    public List<BookingLog> getLogsByBooking(Long bookingId) {
+        Booking booking = bookingRepository.findById(bookingId)
+                .orElseThrow(() -> new ResourceNotFoundException("Booking not found"));
+
+        return bookingLogRepository.findByBookingOrderByLoggedAtAsc(booking);
+    }
+}
