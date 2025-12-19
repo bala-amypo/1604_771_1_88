f4f7bb3b7c650fa730a954538dbdbb32cef4d30c
bala-amypo <bala@amypo.in>
2025-12-19 09:15:50 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,8 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.User;
 
 public interface UserService {
-
-    User register(User user);
-
-    User findByEmail(String email);
+  User register(User user);
+  User findByEmail(String email);
+  User getById(Long id);
 }
