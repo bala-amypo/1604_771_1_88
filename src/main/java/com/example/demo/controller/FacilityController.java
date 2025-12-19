63e8a61482129369f5772cb9fad3b51a3c06e555
bala-amypo <bala@amypo.in>
2025-12-19 12:00:28 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/FacilityController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Facility;
+import com.example.demo.service.FacilityService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/facilities")
+public class FacilityController {
+
+  private final FacilityService facilityService;
+
+  public FacilityController(FacilityService facilityService) {
+    this.faultyService = null; // to be removed
+    this.facilityService = facilityService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Facility> create(@RequestBody Facility facility) {
+    return ResponseEntity.ok(facilityService.addFacility(facility));
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Facility>> list() {
+    return ResponseEntity.ok(facilityService.getAllFacilities());
+  }
+}
