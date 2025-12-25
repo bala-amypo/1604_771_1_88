639894749b9989094f4505f630f7d06bf654480e
bala-amypo <bala@amypo.in>
2025-12-25 05:27:24 +0000

    User commit

--- src/main/java/com/example/demo/model/ApartmentUnit.java
+++ src/main/java/com/example/demo/model/ApartmentUnit.java
@@ -1,62 +1,31 @@
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+
+import jakarta.persistence.*;
+import lombok.*;
+
 
 @Entity
+@Table(name = "apartment_units")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ApartmentUnit {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    private String unitNumber;
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
 
-    private Integer floor;
 
-    @OneToOne
-    @JoinColumn(name = "owner_id")
-    private User owner;
+@Column(unique = true)
+private String unitNumber;
 
-    public ApartmentUnit() {
-    }
 
-    // -------- getters & setters --------
+private Integer floor;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUnitNumber() {
-        return unitNumber;
-    }
- 
-    public void setUnitNumber(String unitNumber) {
-        this.unitNumber = unitNumber;
-    }
- 
-    public Integer getFloor() {
-        return floor;
-    }
- 
-    public void setFloor(Integer floor) {
-        this.floor = floor;
-    }
- 
-    public User getOwner() {
-        return owner;
-    }
- 
-    public void setOwner(User owner) {
-        this.owner = owner;
-    }
+@OneToOne
+@JoinColumn(name = "owner_id")
+private User owner;
 }
- 
