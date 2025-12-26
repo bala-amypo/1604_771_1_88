5c79938c30b3f4215525077dc4bc7ef714cfca7a
bala-amypo <bala@amypo.in>
2025-12-26 13:01:53 +0000

    User commit

--- src/main/java/com/example/demo/repository/FacilityRepository.java
+++ src/main/java/com/example/demo/repository/FacilityRepository.java
@@ -2,7 +2,8 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Facility;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface FacilityRepository extends JpaRepository<Facility, Long> {
-    Facility findByName(String name);
+    Optional<Facility> findByName(String name); // ✅ MUST BE OPTIONAL
 }
