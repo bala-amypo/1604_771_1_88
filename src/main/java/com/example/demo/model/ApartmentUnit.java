3603b1feabd24d0d85fa20dacadd389a980f3849
bala-amypo <bala@amypo.in>
2025-12-26 16:35:23 +0000

    User commit

--- src/main/java/com/example/demo/model/ApartmentUnit.java
+++ src/main/java/com/example/demo/model/ApartmentUnit.java
@@ -1,9 +1,25 @@
 package com.example.demo.model;
-import lombok.Data;
+
+import jakarta.persistence.*;
+import lombok.*;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "apartment_units")
 public class ApartmentUnit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String unitNumber;
-    private User owner; // ⭐ this fixes setOwner()
+
+    private Integer floor;
+
+    @OneToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
 }
