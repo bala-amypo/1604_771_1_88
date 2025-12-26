b41c5638b6ebaa1788003586ea8cc9e4f7308ddb
bala-amypo <bala@amypo.in>
2025-12-26 15:35:22 +0000

    User commit

--- src/main/java/com/example/demo/model/Facility.java
+++ src/main/java/com/example/demo/model/Facility.java
@@ -1,25 +1,12 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
-import lombok.*;
+import lombok.Data;
+import java.time.LocalTime;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Entity
-@Table(name = "facilities")
 public class Facility {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(unique = true)
     private String name;
-
-    private String description;
-
-    private String openTime;
-
-    private String closeTime;
+    private LocalTime openTime;
+    private LocalTime closeTime;
 }
