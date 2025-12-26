c9a565235200e7af13a194a9a71c5085cabe8f3c
bala-amypo <bala@amypo.in>
2025-12-26 15:53:22 +0000

    User commit

--- src/main/java/com/example/demo/service/FacilityService.java
+++ src/main/java/com/example/demo/service/FacilityService.java
@@ -1,5 +1,13 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Facility;
+import java.util.List;
+
 public interface FacilityService {
+
+    Facility addFacility(Facility facility);
+
+    List<Facility> getAllFacilities();
+
     String getFacilityTimings(Long id);
 }
