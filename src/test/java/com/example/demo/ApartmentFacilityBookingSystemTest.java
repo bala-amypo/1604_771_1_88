1fec1a9c7c8c6f271d5d7b8b48b6817a458beb61
bala-amypo <bala@amypo.in>
2025-12-28 17:07:04 +0000

    User commit

--- src/test/java/com/example/demo/ApartmentFacilityBookingSystemTest.java
+++ src/test/java/com/example/demo/ApartmentFacilityBookingSystemTest.java
@@ -3,28 +3,6 @@
 
 
 
-jwt authentication filter
-
-package com.example.demo.security;
-
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import java.io.IOException;
-
-public class JwtAuthenticationFilter implements Filter {
-
-    @Override
-    public void doFilter(ServletRequest request,
-                         ServletResponse response,
-                         FilterChain chain)
-            throws IOException, ServletException {
-        chain.doFilter(request, response);
-    }
-}
-
-
-
-
 jwt token provider
 
 package com.example.demo;
